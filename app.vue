27e29371640b7f2ae33f501a758eece156dcd8bf
Lars van Herk <dev@vanherk.io>
2023-06-01 21:17:43 +0200

    feat: add useBluetooth composable, add testing controls

--- app.vue
+++ app.vue
@@ -1,65 +1,41 @@
 <script setup lang="ts">
+const {
+  isScanning,
+  scanRSSI,
+  requestScan,
+  cancelScan
+} = useBluetooth();
+
 const pup = ref<{
   playbackRate: number
-}>({ playbackRate: 0 });
+  play: Function
+  pause: Function
+}>({ playbackRate: 1, play: () => {}, pause: () => {} });
 
 const speed = computed({
   get() {
     return pup.value.playbackRate;
   },
   set(rate) {
-    pup.value.playbackRate = rate;
+    if (rate <= 0.7) {
+      pup.value.playbackRate = 0.7;
+      pup.value.pause();
+    } else {
+      pup.value.playbackRate = rate;
+      pup.value.play();
+    }
   }
 });
 
-const logDataView = (labelOfDataSource: any, key: any, valueDataView: any) => {
-  const hexString = [...new Uint8Array(valueDataView.buffer)].map(b => {
-    return b.toString(16).padStart(2, '0');
-  }).join(' ');
-  const textDecoder = new TextDecoder('ascii');
-  const asciiString = textDecoder.decode(valueDataView.buffer);
-  console.log(`  ${labelOfDataSource} Data: ` + key +
-      '\n    (Hex) ' + hexString +
-      '\n    (ASCII) ' + asciiString);
-};
+const isPartying = computed(() => scanRSSI.value >= -60);
 
-const requestScan = () => {
-  const { bluetooth } = navigator as Navigator & { bluetooth: { requestLEScan: Function, addEventListener: Function } };
+watch(isPartying, () => {
+  speed.value = isPartying.value ? 2 : 0;
+});
 
-  try {
-    console.log('Requesting Bluetooth Scan');
-    const scan = bluetooth.requestLEScan({ filters: [{ services: [0xFEAA] }] });
-
-    console.log('Scan started with:');
-    console.log(' acceptAllAdvertisements: ' + scan.acceptAllAdvertisements);
-    console.log(' active: ' + scan.active);
-    console.log(' keepRepeatedDevices: ' + scan.keepRepeatedDevices);
-    console.log(' filters: ' + JSON.stringify(scan.filters));
-
-    bluetooth.addEventListener('advertisementreceived', (event: any) => {
-      console.log('Advertisement received.');
-      console.log('  Device Name: ' + event.device.name);
-      console.log('  Device ID: ' + event.device.id);
-      console.log('  RSSI: ' + event.rssi);
-      console.log('  TX Power: ' + event.txPower);
-      console.log('  UUIDs: ' + event.uuids);
-      event.manufacturerData.forEach((valueDataView: any, key: any) => {
-        logDataView('Manufacturer', key, valueDataView);
-      });
-      event.serviceData.forEach((valueDataView: any, key: any) => {
-        logDataView('Service', key, valueDataView);
-      });
-    });
-
-    setTimeout(() => {
-      console.log('Stopping scan...');
-      scan.stop();
-      console.log('Stopped.  scan.active = ' + scan.active);
-    }, 10000);
-  } catch(error)  {
-    console.log('Argh! ' + error);
-  }
-};
+onMounted(() => {
+  speed.value = 0;
+});
 </script>
 
 <template>
@@ -78,11 +54,25 @@ const requestScan = () => {
       v-model="speed"
       type="range"
       step="0.01"
-      min="0"
-      max="1">
+      min="0.7"
+      max="2">
 
-    <button @click="requestScan">
-      Request Scan
-    </button>
+    <pre>{{ { scanRSSI, isPartying } }}</pre>
+
+    <div class="flex flex-row gap-5">
+      <button
+        v-if="!isScanning"
+        @click="requestScan">
+        Request Scan
+      </button>
+      <button
+        v-else
+        @click="() => cancelScan()">
+        Cancel Scan
+      </button>
+      <button @click="pup.play()">
+        Force play
+      </button>
+    </div>
   </main>
 </template>
---
+++ composables/useBluetooth.ts
@@ -0,0 +1,94 @@
+/* eslint-disable no-console */
+
+const { bluetooth } = process.client
+  ? navigator as Navigator & { bluetooth: { requestLEScan: Function, addEventListener: Function } }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  : { bluetooth: { requestLEScan: (options: any) => {}, addEventListener: (name: string, handler: Function) => {} } };
+
+
+let scan: {
+  stop: Function
+  filters: any
+  active: boolean
+};
+
+const scanRSSI = ref(-999);
+
+const logDataView = (valueDataView: any) => {
+  const textDecoder = new TextDecoder('ascii');
+  const asciiString = textDecoder.decode(valueDataView.buffer);
+  console.log('  Beacon ASCII Data:', asciiString);
+};
+
+bluetooth.addEventListener('advertisementreceived', (event: any) => {
+  console.log('### Advertisement received ###');
+  console.log('  Device ID: ' + event.device.id);
+  console.log('  RSSI: ' + event.rssi);
+  event.serviceData.forEach(logDataView);
+
+  scanMonitor.retries = 0;
+
+  scanRSSI.value = event.rssi;
+});
+
+const scanMonitor: { retries: number, start: Function, kill: Function, handlerID?: NodeJS.Timer } = {
+  retries: 0,
+  start: (abort: Function) => {
+    scanMonitor.kill();
+    
+    scanMonitor.retries = 0;
+    scanMonitor.handlerID = setInterval(() => {
+      console.log('[useBluetooth] Is scan still active:', scan.active);
+      scanMonitor.retries++;
+
+      if (scanMonitor.retries >= 40) {
+        abort(true);
+      }
+    }, 200);
+  },
+  kill: () => {
+    if (scanMonitor.handlerID) {
+      clearTimeout(scanMonitor.handlerID);
+    }
+  },
+  handlerID: undefined
+};
+
+export const useBluetooth = () => {
+  const isScanning = ref(false);
+
+  const requestScan = async () => {
+    cancelScan();
+
+    try {
+      console.log('[useBluetooth] Requesting Bluetooth Scan');
+      scan = await bluetooth.requestLEScan({ filters: [{ services: [0xFEAA] }] });
+      console.log('[useBluetooth] Scan started...');
+      isScanning.value = true;
+
+      scanMonitor.start(cancelScan);
+    } catch(error)  {
+      console.log('[useBluetooth] Scan init failed:', error);
+      isScanning.value = false;
+    }
+  };
+
+  const cancelScan = (aborted: boolean = false) => {
+    console.log(`[useBluetooth] Cancelling scan... (active: ${scan?.active})`);
+    if (aborted) console.warn('[useBluetooth] Scan was aborted, because it probably died');
+    if (scan) {
+      scan.stop();
+      scanMonitor.kill();
+    }
+    scanRSSI.value = -999;
+    isScanning.value = false;
+    console.log('[useBluetooth] Scan cancelled');
+  };
+
+  return {
+    isScanning,
+    scanRSSI,
+    requestScan,
+    cancelScan
+  };
+};
